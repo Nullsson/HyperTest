d0354afa6888f639ab157e18a8e1bf5ade662df4
Oskar Mendel <brokenprogrammer@gmail.com>
2020-12-30 18:00:47 +0100

    Added unittest initializer for others than msvc.

--- code/hypertest.h
+++ code/hypertest.h
@@ -12,6 +12,10 @@
   static void __cdecl f(void);                                                 \
   __declspec(allocate(".CRT$XCU")) void(__cdecl * f##_)(void) = f;             \
 static void __cdecl f(void)
+#else
+#define UNITTEST_INITIALIZER(f) \
+        static void f(void) __attribute__((constructor)); \
+        static void f(void)
 #endif
 
 typedef void (*Unittest_Testcase)(int *, size_t);
