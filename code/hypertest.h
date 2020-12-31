6f07dca6559444cc5c413826c1677872e4f93ccc
Oskar Mendel <brokenprogrammer@gmail.com>
2020-12-31 11:03:20 +0100

    Updated initializer macro for unittest.

--- code/hypertest.h
+++ code/hypertest.h
@@ -6,14 +6,37 @@
 #include "string.h"
 
 // NOTE: CRT hack
-#if defined(_MSC_VER)
-#pragma section(".CRT$XCU", read)
-#define UNITTEST_INITIALIZER(f)                                                \
-  static void __cdecl f(void);                                                 \
-  __declspec(allocate(".CRT$XCU")) void(__cdecl * f##_)(void) = f;             \
-static void __cdecl f(void)
+// #if defined(_MSC_VER)
+// #pragma section(".CRT$XCU", read)
+// #define UNITTEST_INITIALIZER(f)                                                \
+//   static void __cdecl f(void);                                                 \
+//   __declspec(allocate(".CRT$XCU")) void(__cdecl * f##_)(void) = f;             \
+// static void __cdecl f(void)
+// #else
+// #define UNITTEST_INITIALIZER(f) \
+//         static void f(void) __attribute__((constructor)); \
+//         static void f(void)
+// #endif
+
+#ifdef __cplusplus
+    #define UNITTEST_INITIALIZER(f) \
+        static void f(void); \
+        struct f##_t_ { f##_t_(void) { f(); } }; static f##_t_ f##_; \
+        static void f(void)
+#elif defined(_MSC_VER)
+    #pragma section(".CRT$XCU",read)
+    #define UNITTEST_INITIALIZER2_(f,p) \
+        static void f(void); \
+        __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+        __pragma(comment(linker,"/include:" p #f "_")) \
+        static void f(void)
+    #ifdef _WIN64
+        #define UNITTEST_INITIALIZER(f) UNITTEST_INITIALIZER2_(f,"")
+    #else
+        #define UNITTEST_INITIALIZER(f) UNITTEST_INITIALIZER2_(f,"_")
+    #endif
 #else
-#define UNITTEST_INITIALIZER(f) \
+    #define UNITTEST_INITIALIZER(f) \
         static void f(void) __attribute__((constructor)); \
         static void f(void)
 #endif
