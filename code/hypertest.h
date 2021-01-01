4a2b32e13ef9b65b224f6e3784a328153047db06
Oskar Mendel <brokenprogrammer@gmail.com>
2021-01-01 11:08:09 +0100

    Updated formatting of code.

--- code/hypertest.h
+++ code/hypertest.h
@@ -5,40 +5,28 @@
 #include "stdlib.h"
 #include "string.h"
 
-// NOTE: CRT hack
-// #if defined(_MSC_VER)
-// #pragma section(".CRT$XCU", read)
-// #define UNITTEST_INITIALIZER(f)                                                \
-//   static void __cdecl f(void);                                                 \
-//   __declspec(allocate(".CRT$XCU")) void(__cdecl * f##_)(void) = f;             \
-// static void __cdecl f(void)
-// #else
-// #define UNITTEST_INITIALIZER(f) \
-//         static void f(void) __attribute__((constructor)); \
-//         static void f(void)
-// #endif
-
+// NOTE(Oskar): Pre-main execution of functions.
 #ifdef __cplusplus
-    #define UNITTEST_INITIALIZER(f) \
-        static void f(void); \
-        struct f##_t_ { f##_t_(void) { f(); } }; static f##_t_ f##_; \
-        static void f(void)
+#define UNITTEST_INITIALIZER(f) \
+    static void f(void); \
+    struct f##_t_ { f##_t_(void) { f(); } }; static f##_t_ f##_; \
+    static void f(void)
 #elif defined(_MSC_VER)
-    #pragma section(".CRT$XCU",read)
-    #define UNITTEST_INITIALIZER2_(f,p) \
-        static void f(void); \
-        __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
-        __pragma(comment(linker,"/include:" p #f "_")) \
-        static void f(void)
-    #ifdef _WIN64
-        #define UNITTEST_INITIALIZER(f) UNITTEST_INITIALIZER2_(f,"")
-    #else
-        #define UNITTEST_INITIALIZER(f) UNITTEST_INITIALIZER2_(f,"_")
-    #endif
+#pragma section(".CRT$XCU",read)
+#define UNITTEST_INITIALIZER2_(f,p) \
+    static void f(void); \
+    __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+    __pragma(comment(linker,"/include:" p #f "_")) \
+    static void f(void)
+#ifdef _WIN64
+#define UNITTEST_INITIALIZER(f) UNITTEST_INITIALIZER2_(f,"")
 #else
-    #define UNITTEST_INITIALIZER(f) \
-        static void f(void) __attribute__((constructor)); \
-        static void f(void)
+#define UNITTEST_INITIALIZER(f) UNITTEST_INITIALIZER2_(f,"_")
+#endif
+#else
+#define UNITTEST_INITIALIZER(f) \
+    static void f(void) __attribute__((constructor)); \
+    static void f(void)
 #endif
 
 typedef void (*Unittest_Testcase)(int *, size_t);
